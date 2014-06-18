ae7648e2026d8219d7f78505514ec85de31d9e54
Steven Chen <ejsteven@gmail.com>
2014-06-18 18:50:44 +1000

    Modified line chart to handle multiple series

--- app/scripts/directives/line/line.js
+++ app/scripts/directives/line/line.js
@@ -9,36 +9,95 @@
  */
 
 app.directive('visLine', function() {
+	var dataMaxVal = function(data) {
+		var maxVal, key, dataSetMax, first = true;
+
+		for (key in data) {
+			dataSetMax = d3.max(data[key].data, function(d) { return d.value });
+
+			if (first) {
+				first = false;
+				maxVal = dataSetMax;
+			}
+			else if (dataSetMax > maxVal) {
+				maxVal = dataSetMax;
+			}
+		}
+
+		return maxVal;
+	};
+
+	var dataMinVal = function(data) {
+		var minVal, key, dataSetMin, first = true;
+
+		for (key in data) {
+			dataSetMin = d3.min(data[key].data, function(d) { return d.value });
+
+			if (first) {
+				first = false;
+				minVal = dataSetMin;
+			}
+			else if (dataSetMin < minVal) {
+				minVal = dataSetMin;
+			}
+		}
+
+		return minVal;
+	};
+
+	var dataExtentDate = function(data) {
+		var dateExtent, key, dataSetExtent, first = true;
+
+		for (key in data) {
+			dataSetExtent = d3.extent(data[key].data, function(d) { return d.date });
+
+			if (first) {
+				first = false;
+				dateExtent = dataSetExtent;
+			}
+			else {
+				if (dataSetExtent[0] < dateExtent[0]) {
+					dateExtent[0] = dataSetExtent[0];
+				}
+
+				if (dataSetExtent[1] > dateExtent[1]) {
+					dateExtent[1] = dataSetExtent[1];
+				}
+			}
+		}
+
+		return dateExtent;
+	};
+
 	return {
 		restrict: 'EA',
 		scope: {
-			data: '=data',
+			chartData: '=data',
 			width: '=width',
 			height: '=height',
 		},
 		templateUrl: 'scripts/directives/line/line.html',
 		link: function(scope, element) {
-			scope.chartData = [];
-			scope.chartDataTwo = [];
-
 			var parseDate = d3.time.format('%Y-%m-%d').parse;
 
-			for (var i in scope.data) {
-				scope.chartData.push({ date: parseDate(scope.data[i].x), close: scope.data[i].y });
-			}
-			for (var i in scope.data) {
-				scope.chartDataTwo.push({ date: parseDate(scope.data[i].x), close: (scope.data[i].y + 10) });
+			for (var i in scope.chartData) {
+				for (var j in scope.chartData[i].data) {
+					scope.chartData[i].data[j].date = parseDate(scope.chartData[i].data[j].date);
+				}
 			}
 
-			var margin = { top: 40, right: 40, bottom: 60, left: 100 },
-				width = 960 - margin.left - margin.right,
-				height = 500 - margin.top - margin.bottom;
+			var margin = { top: 40, right: 200, bottom: 60, left: 100 },
+				width = scope.width - margin.left - margin.right,
+				height = scope.height - margin.top - margin.bottom;
+
+				console.log(dataExtentDate(scope.chartData));
 
 			var x = d3.time.scale()
+				.domain(dataExtentDate(scope.chartData))
 				.range([0, width]);
 
 			var y = d3.scale.linear()
-				.domain([0, d3.max(scope.chartData, function(d) { return d.close })])
+				.domain([0, dataMaxVal(scope.chartData)])
 				.range([height, 0]);
 
 			var xAxis = d3.svg.axis()
@@ -53,7 +112,7 @@ app.directive('visLine', function() {
 
 			var line = d3.svg.line()
 				.x(function(d) { return x(d.date) })
-				.y(function(d) { return y(d.close) });
+				.y(function(d) { return y(d.value) });
 
 			var svg = d3.select(element.find('.line-chart')[0])
 				.attr('width', width + margin.left + margin.right)
@@ -61,7 +120,27 @@ app.directive('visLine', function() {
 				.append('g')
 				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-			x.domain(d3.extent(scope.chartData, function(d) { return d.date }));
+			var color = d3.scale.category10();
+
+			var tip = d3.tip()
+  				.attr('class', 'd3-tip')
+				.offset([-10, 0])
+				.html(function(d, name) {
+					return '<strong>Date:</strong>' +
+					'<span style="color: ' + color(name) +'">' + ('0' + d.date.getDate()).slice(-2) + '-' + ('0' + (d.date.getMonth()+1)).slice(-2) + '-' + d.date.getFullYear() + '</span>' +
+					'<strong>Value:</strong>' +
+					'<span style="color: ' + color(name) +'">' + d.value + '</span>';
+				})
+
+			svg.call(tip);
+
+			var nameList = [];
+
+			scope.chartData.forEach(function(d) {
+				nameList.push(d.name);
+			});
+
+			color.domain(nameList);
 
 			svg.append('g')
 				.attr('class', 'x axis')
@@ -79,53 +158,66 @@ app.directive('visLine', function() {
 				.style('text-anchor', 'end')
 				.text('Num. Registration');
 
-			svg.append('path')
-				.datum(scope.chartData)
-				.attr('class', 'line')
-				.attr('d', line);
-
-			svg.append('path')
-				.datum(scope.chartDataTwo)
-				.attr('class', 'line')
-				.attr('d', line);
-
-			svg.selectAll('circle')
+			var series = svg.selectAll('.series')
 				.data(scope.chartData)
+				.enter().append('g')
+				.attr('class', 'series');
+
+			series.append('path')
+				.attr('class', 'line')
+				.attr('d', function(d) { return line(d.data) })
+				.style('stroke', function(d) { return color(d.name) });
+
+			series.selectAll('.point')
+				.data(function(d) { return d.data })
 				.enter().append('circle')
 				.attr('class', 'point')
 				.attr('cx', function(d) { return x(d.date) })
-				.attr('cy', function(d) { return y(d.close) })
+				.attr('cy', function(d) { return y(d.value) })
 				.attr('r', 3)
-				.on('mouseover', function(d) {
-					var chartTip = $($(this).closest('vis-line').find('.tip')[0]);
-					var dateFormat = d3.time.format('%Y-%m-%d').parse;
-
-					chartTip.css('left', (event.clientX + 15) + 'px' );
-					chartTip.css('top', (event.clientY - 45) + 'px');
-
-					chartTip.html(
-						'<dl>' +
-							'<dt>Date:</dt><dd>' + ('0' + d.date.getDate()).slice(-2) + '-' + ('0' + (d.date.getMonth()+1)).slice(-2) + '-' + d.date.getFullYear() + '</dd>' +
-							'<dt>Registrations:</dt><dd>' + d.close + '</dd>' +
-						'</dl>'
-					);
-					chartTip.fadeIn(100);
-
-					d3.select(this)
-						.transition()
-						.duration(200)
-						.attr('r', 5);
+				.attr('fill', function(d) {
+					return color(d3.select(this.parentNode).datum().name);
 				})
-				.on('mouseout', function() {
-					var chartTip = $($(this).closest('vis-line').find('.tip')[0]);
-
-					chartTip.fadeOut(50);
-
+				.on('mouseover', function(d) {
 					d3.select(this)
-						.transition()
-						.duration(200)
+				 		.transition()
+				 		.duration(200)
+						.attr('r', 5);
+
+					tip.show(d, d3.select(this.parentNode).datum().name);
+				})
+				.on('mouseout', function(d) {
+					d3.select(this)
+				 		.transition()
+				 		.duration(200)
 						.attr('r', 3);
+
+					tip.hide(d, d3.select(this.parentNode).datum().name);
 				});
+
+			var legend = d3.select(element.find('.line-chart')[0]).append('g')
+				.attr('class', 'legend')
+				.selectAll('.series-legend')
+        		.data(scope.chartData)
+        		.enter().append('g')
+        		.attr('class', 'series-legend');
+
+    		legend.append('rect')
+        		.attr('x', width + margin.left + 40)
+        		.attr('y', function(d, i) {
+        			console.log(i);
+        			return i * 30 + margin.top
+        		})
+        		.attr('width', 20)
+        		.attr('height', 20)
+        		.style('fill', function(d) { return color(d.name) });
+
+    		legend.append('text')
+        		.attr('x', width + margin.left + 70)
+        		.attr('y', function(d, i) {
+        			return (i *  30) + 15 + margin.top
+        		})
+        		.text(function(d) { return d.name });
 		}
 	};
 });
--- app/visualisations/enrolment/controller.js
+++ app/visualisations/enrolment/controller.js
@@ -8,6 +8,18 @@
  * Controller of the dashboardJsApp
  */
 angular.module('dashboardJsApp')
-  .controller('Visualisation_enrolment_Ctrl', function ($scope) {
-    $scope.newRegData = [{ x: '1987-01-04', y: 5 }, { x: '1987-01-05', y: 20 }, { x: '1987-01-06', y: 10 }, { x: '1987-01-07', y: 40 }, { x: '1987-01-08', y: 5 }, { x: '1987-01-09', y: 60 }];
-  });
+	.controller('Visualisation_enrolment_Ctrl', function ($scope) {
+		$scope.newRegData = [{
+    		name: 'Total' ,
+    		data: [{ date: '1987-01-04', value: 5 }, { date: '1987-01-05', value: 20 }, { date: '1987-01-06', value: 10 }, { date: '1987-01-07', value: 40 }, { date: '1987-01-08', value: 5 }, { date: '1987-01-09', value: 60 }]
+    	}, {
+    		name: 'Still Enrolled',
+    		data: [{ date: '1987-01-04', value: 15 }, { date: '1987-01-05', value: 10 }, { date: '1987-01-06', value: 40 }, { date: '1987-01-07', value: 30 }, { date: '1987-01-08', value: 5 }, { date: '1987-01-09', value: 15 }]
+    	}, {
+    		name: 'Another Line',
+    		data: [{ date: '1987-01-04', value: 20 }, { date: '1987-01-05', value: 30 }, { date: '1987-01-06', value: 20 }, { date: '1987-01-07', value: 22 }, { date: '1987-01-08', value: 13 }, { date: '1987-01-09', value: 14 }]
+    	}, {
+    		name: 'Yet Another',
+    		data: [{ date: '1987-01-02', value: 10 }, { date: '1987-01-05', value: 20 }, { date: '1987-01-06', value: 5 }, { date: '1987-01-07', value: 16 }, { date: '1987-01-08', value: 15 }, { date: '1987-01-09', value: 45 }]
+    	}];
+  	});
