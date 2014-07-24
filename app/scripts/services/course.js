578cd61719f61ab3fc0a60ceb1485abbf362d373
Andrew Dekker <uqadekke@uq.edu.au>
2014-07-24 11:59:19 +1000

    All button

--- app/scripts/services/course.js
+++ app/scripts/services/course.js
@@ -13,6 +13,9 @@ angular.module('dashboardJsApp')
 		this.courseList = [];
 
 		this.setCourseList = function(courseList) {
+            console.log("SETTIG COURSE LIST"+courseList);
+            courseList.splice(0, 0, {'icon':'fa-dashboard', 'id':'allcourses', 'name':'All Courses List'});
+            console.log(courseList);
 			for (var i in courseList) {
 				var shortName = courseList[i].name.split(" ");
 				shortName.pop();
@@ -25,6 +28,7 @@ angular.module('dashboardJsApp')
 		};
 
 		this.getCurrentCourseShortName = function() {
+            console.log("GETTING SHORT NAME FOR");
 			for (var key in this.courseList) {
 				if (this.courseList[key].id === this.currentCourse) {
 					return this.courseList[key].shortName;
@@ -35,6 +39,7 @@ angular.module('dashboardJsApp')
 		};
 
 		this.getCourseIds = function() {
+            console.log("GETTING ID");
 			var courseIds = [];
 
 			for (var key in this.courseList) {
@@ -45,6 +50,7 @@ angular.module('dashboardJsApp')
 		};
 
 		this.getCourseNameFromId = function(courseId) {
+            console.log("GETTING NAME");
 			for (var key in this.courseList) {
 				if (this.courseList[key].id === courseId) {
 					return this.courseList[key].name;
@@ -55,6 +61,7 @@ angular.module('dashboardJsApp')
 		};
 
 		this.getCourseIconFromId = function(courseId) {
+            console.log("GETTING ICON");
 			for (var key in this.courseIcon) {
 				if (this.courseList[key].id === courseId) {
 					return this.courseList[key].name;
