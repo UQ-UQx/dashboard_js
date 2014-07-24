8964dbfebf9e52342f925045002c0d38c80a2dc2
Steven Chen <ejsteven@gmail.com>
2014-07-24 15:06:05 +1000

    Discussion forum map real data

--- app/visualisations/enrolmentmap/controller.js
+++ app/visualisations/enrolmentmap/controller.js
@@ -9,52 +9,50 @@
  */
 angular.module('dashboardJsApp')
 	.controller('Visualisation_enrolmentmap_Ctrl', ['$scope', 'RequestService', 'Course', 'AuthService', 'COUNTRY', function ($scope, RequestService, Course, AuthService, COUNTRY) {
-        $scope.enrolmentData = [];
-        $scope.populationData = [];
-        $scope.auth = AuthService;
+		$scope.enrolmentData = [];
+		$scope.populationData = [];
+		$scope.auth = AuthService;
 		$scope.colourString = '23, 60, 68';
 
-        $scope.$parent.state = "loading";
+		$scope.$parent.state = "loading";
 
-        $scope.formatData = function() {
+		$scope.$watch('auth.isAuthenticated()', function() {
 
-            var twodtoname = {};
-            for(var country in COUNTRY) {
-                twodtoname[COUNTRY[country]['alpha-2']] = COUNTRY[country]['name'];
-            }
+			var twodtoname = {};
+			for(var country in COUNTRY) {
+				twodtoname[COUNTRY[country]['alpha-2']] = COUNTRY[country]['name'];
+			}
 
-            if ($scope.auth.isAuthenticated()) {
-                RequestService.async('http://api.uqxdev.com/api/students/countries/' + Course.currentCourse + '/').then(function(data) {
-                    $scope.populationData = [];
-                    $scope.enrolmentData = [];
-                    var tmpEnrolmentData = [];
-                    for(var country in data) {
-                        if(country != 'null') {
-                            var roundedPercentage = Math.round(data[country]['percentage']*100)/100;
-                            var popObject = {'country': country, 'value': data[country]['count'], 'percentage': roundedPercentage};
-                            $scope.populationData.push(popObject);
-                            var enrolObject = [twodtoname[country], roundedPercentage, data[country]['count']];
-                            tmpEnrolmentData.push(enrolObject);
-                        }
-                    }
-                    function percentage_compare(a,b) {
-                      if (a[1] > b[1])
-                         return -1;
-                      if (a[1] < b[1])
-                        return 1;
-                      return 0;
-                    }
-                    tmpEnrolmentData.sort(percentage_compare);
-                    for(country in tmpEnrolmentData) {
-                        if(tmpEnrolmentData[country][1] < 1) {
-                            break;
-                        }
-                        $scope.enrolmentData.push(tmpEnrolmentData[country]);
-                    }
-                });
-                $scope.$parent.state = "running";
-            }
-        };
-
-        $scope.$watch('course.currentCourse', $scope.formatData());
-  	}]);
+			if ($scope.auth.isAuthenticated()) {
+				RequestService.async('http://api.uqxdev.com/api/students/countries/' + Course.currentCourse + '/').then(function(data) {
+					$scope.populationData = [];
+					$scope.enrolmentData = [];
+					var tmpEnrolmentData = [];
+					for(var country in data) {
+						if(country != 'null') {
+							var roundedPercentage = Math.round(data[country]['percentage']*100)/100;
+							var popObject = {'country': country, 'value': data[country]['count'], 'percentage': roundedPercentage};
+							$scope.populationData.push(popObject);
+							var enrolObject = [twodtoname[country], roundedPercentage, data[country]['count']];
+							tmpEnrolmentData.push(enrolObject);
+						}
+					}
+					function percentage_compare(a,b) {
+					  if (a[1] > b[1])
+						 return -1;
+					  if (a[1] < b[1])
+						return 1;
+					  return 0;
+					}
+					tmpEnrolmentData.sort(percentage_compare);
+					for(country in tmpEnrolmentData) {
+						if(tmpEnrolmentData[country][1] < 1) {
+							break;
+						}
+						$scope.enrolmentData.push(tmpEnrolmentData[country]);
+					}
+				});
+				$scope.$parent.state = "running";
+			}
+		});
+	}]);
